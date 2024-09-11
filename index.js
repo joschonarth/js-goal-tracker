57b95cabab906648720f3b1ec8f20e2917bc8f78
joschonarth <joschonarth@gmail.com>
2024-09-11 09:13:40 -0300

    Update NLW Pocket JS

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const { select, input } = require('@inquirer/prompts')
+const { select, input, checkbox } = require('@inquirer/prompts')
+
+let meta = {
+    value: "Tomar água",
+    checked: false,
+}
 
 let metas = [ meta ]
 
@@ -14,6 +19,33 @@ const cadastrarMeta = async () => {
     metas.push(
         { value: meta, checked: false}
     )
+}
+
+const listarMetas = async () => {
+    const respostas = await checkbox({
+        message: "Use as setas para mudar de meta, o espaço para marcar e desmarcar e o Enter para finalizar essa etapa",
+        choices: [...metas],
+        instructions: false,
+    })
+    
+    if(respostas.length == 0) {
+        console.log("Nenhuma meta selecionada!")
+        return
+    }
+
+    metas.forEach((m) => {
+        m.checked = false
+    })
+
+    respostas.forEach((resposta) => {
+        const meta = metas.find((m) => {
+            return m.value == resposta
+        })
+
+        meta.checked = true
+    })
+
+    console.log('Meta(s) marcada(s) como concluída(s)')
 }
 
 const start = async () => {
@@ -44,7 +76,7 @@ const start = async () => {
                 console.log(metas)
                 break
             case "listar":
-                console.log("listar")
+                await listarMetas()
                 break
             case "sair":
                 console.log("sair")
@@ -53,3 +85,5 @@ const start = async () => {
     }
 
 }
+
+start()
