350593f333580a29a0bb952caf487dc5fa77166c
joschonarth <joschonarth@gmail.com>
2024-09-11 08:28:13 -0300

    Update NLW Pocket JS

---
+++ index.js
@@ -0,0 +1,26 @@
+const { select } = require('@inquirer/prompts')
+
+const start = async () => {
+
+    while(true) {
+
+        const opcao = await select({
+            message: "Menu >",
+            choices: [
+                {
+                    name: "Cadastrar meta",
+                    value: "cadastrar"
+                },
+                {
+                    name: "Listar metas",
+                    value: "listar"
+                },
+                {
+                    name: "Sair",
+                    value: "sair"
+                }
+            ]
+        })
+    }
+
+}
